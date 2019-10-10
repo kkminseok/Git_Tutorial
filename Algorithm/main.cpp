6fe637b0fdf8d4c901fce7b53a0fef19608d98f6
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-10-10 18:39:17 +0900

    swea8556

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,35 +1,113 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<iostream>
+#include<string>
+#include<stack>
+#include<math.h>
+
 
 
 int main()
 {
 	int T;
-	scanf("%d", &T);
-	getchar();
-
+	std::cin >> T;
+	std::cin.ignore(1000,'\n');
 	for (int i = 0; i < T; ++i)
 	{
-		int N;
-		scanf("%d", &N);
-		getchar();
+		
 		double result = 0;
-		double* p = (double*)calloc(sizeof(double),N);
-		int* x = (int*)calloc(sizeof(int),N);
-
-		for (int j = 0; j < N; ++j)
+		std::stack<char> st1;
+		std::string str;
+		std::getline(std::cin, str);
+		
+		
+		for (int j = 0; j < str.size(); ++j)
 		{
-			scanf("%lf %d", &p[j], &x[j]);
-			printf("%lf %d\n", p[j], x[j]);
-			result += p[j] * x[j];
+			if (str[j] == 'n')
+			{
+				st1.push('n');
+			}
+			else if (str[j] == 'w')
+				st1.push('w');
 		}
-		printf("%.5lf\n", result);
-
-
-		free(p);
-		free(x);
+		
+		int count = 0;
+		if (st1.size() == 1)
+		{
+			st1.top() == 'n' ? std::cout <<"#"<<i+1<<" "<< 0 << std::endl : std::cout <<"#"<<i+1<<" "<< 90 << std::endl;
+			continue;
+		}
+		int beforen;
+		int befored;
+		int result2 = 0;
+		while (st1.empty() == 0)
+		{
+			int numerator = 90;// 분자
+			int denominator = (int)pow(2, count);
+	
+			if (count == 0 && st1.top() == 'n')
+			{
+				beforen = 0;
+				befored = 1;
+				++count;
+				st1.pop();
+				continue;
+			}
+			else if (count == 0 && st1.top() == 'w')
+			{
+				beforen = 90;
+				befored = 1;
+				++count;
+				st1.pop();
+				continue;
+			}
+			if (st1.top() == 'n')
+			{
+				
+				//기약분수로 바꿈
+				while (numerator % 2 == 0 && denominator % 2 == 0)
+				{
+					numerator /= 2;
+					denominator /= 2;
+				}
+				while (befored != denominator)
+				{
+					befored *= 2;
+					beforen *= 2;
+				}
+				result2 = beforen - numerator;
+				befored = denominator;
+				beforen = result2;
+			
+			}
+			else if (st1.top() == 'w')
+			{
+				//기약분수로 바꿈
+				if (numerator % 2 == 0 && denominator % 2 == 0)
+				{
+					numerator /= 2;
+					denominator /= 2;
+				}
+				while (befored != denominator)
+				{
+					befored *= 2;
+					beforen *= 2;
+				}
+				result2 = beforen + numerator;
+				befored = denominator;
+				beforen = result2;
+				
+			}
+			
+			st1.pop();
+			++count;
+	
+		}
+		if (befored != 1)
+			std::cout <<"#"<<i+1<<" "<< result2 << "/" << befored << std::endl;
+		else
+			std::cout << "#" << i + 1 << " " << result2 << std::endl;
+		
 	}
-
 
 	return 0;
 }
