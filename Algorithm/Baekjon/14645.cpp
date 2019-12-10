0ebfd86f20a415e0fbab0f1a22df3ca41ef23448
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-12-10 14:37:47 +0900

    쉬운문제
    forever Bewhy

---
+++ Algorithm/Baekjon/14645.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<stdlib.h>
+#include<functional>
+#include<cmath>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	int n,k;
+	scanf("%d %d",&n,&k);
+	getchar();
+	for(int i=0;i<n;++i)
+	{
+		int a,b;
+		scanf("%d %d",&a,&b);
+		
+	}
+	puts("비와이");
+	
+	
+}
