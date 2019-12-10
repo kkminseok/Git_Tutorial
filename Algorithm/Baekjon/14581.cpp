ba04eca8ad3f6bc7637ef09a47dbc828bf5181f9
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-12-10 23:32:31 +0900

    초보문제

---
+++ Algorithm/Baekjon/14581.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<stdlib.h>
+#include<functional>
+#include<cmath>
+#include<ctime>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	string id;
+	getline(cin,id);
+	cout<<":fan:"<<":fan:"<<":fan:"<<endl;
+	cout<<":fan::"<<id<<"::fan:"<<endl;
+	cout<<":fan:"<<":fan:"<<":fan:"<<endl;
+	
+	
+}
