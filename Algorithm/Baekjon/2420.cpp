077ba99c17a06a117e2ead8c42dea997530ef73c
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-12-23 22:17:48 +0900

    초보문제_사파리월드
    자료형때문에 정답률을 많이 깎아먹은듯.

---
+++ Algorithm/Baekjon/2420.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<stdlib.h>
+#include<functional>
+#include<cmath>
+#include<ctime>
+#include<vector>
+#include<ctype.h>
+using namespace std;
+
+
+
+int main()
+{
+	long long n,m;
+	scanf("%lld %lld",&n,&m);
+	if(n-m<0)
+		printf("%lld",-(n-m));
+	else
+		printf("%lld",n-m);
+	
+	
+	
+	
+}
