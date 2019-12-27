7fa281db0c2129e7857ddcee12633a8a73ec064d
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-12-27 23:32:32 +0900

    초보문제_R2
    수학초보문제

---
+++ Algorithm/Baekjon/3046.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<stdlib.h>
+#include<functional>
+#include<cmath>
+#include<ctime>
+#include<vector>
+#include<ctype.h>
+#include<utility>
+using namespace std;
+
+int main()
+{
+	int r1,s;
+	scanf("%d %d",&r1,&s);
+	printf("%d\n",s*2-r1);
+}
