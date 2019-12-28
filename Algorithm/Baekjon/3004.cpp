48ae6aef690ad1e2727662774abcbc6e2571b277
kkminseok <30401054+kkminseok@users.noreply.github.com>
2019-12-28 23:34:32 +0900

    초보문제_체스판_조각
    메모장이 없어서 생각만으로 풀기는 힘들었다.

---
+++ Algorithm/Baekjon/3004.cpp
@@ -0,0 +1,19 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+#include<stdio.h>
+#include<stdlib.h>
+#include<functional>
+#include<cmath>
+#include<ctime>
+#include<vector>
+#include<ctype.h>
+#include<utility>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",(n/2+1)*((n-n/2)+1));
+}
