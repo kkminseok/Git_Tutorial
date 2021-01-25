3a3a6408886fff39bd4f816a6d55c7ae5b793a70
kkminseok <30401054+kkminseok@users.noreply.github.com>
2021-01-25 16:36:28 +0900

    NoviceDP_1309

---
+++ Algorithm/Baekjoon/1309.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+const int MAX = 100002;
+int DP[MAX][4] = { 0, };
+
+void printDP(int n)
+{
+	for (int i = 0; i <= n; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+			cout << DP[i][j] << " ";
+		cout << '\n';
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	DP[1][0] = 1;
+	DP[1][1] = 1;
+	DP[1][2] = 1;
+	for (int i = 2; i <= n; ++i)
+	{
+		//경우 1 미배치 하는 경우
+		DP[i][0] = (DP[i - 1][0] + DP[i - 1][1] + DP[i - 1][2]) % 9901;
+		//경우 2 왼쪽 칸에 사자를 배치할 경우
+		DP[i][1] = (DP[i - 1][0] + DP[i - 1][2]) % 9901;
+		//경우 3 오른쪽 칸에 사자를 배치할 경우
+		DP[i][2] = (DP[i - 1][0] + DP[i - 1][1]) % 9901;
+	}
+	printDP(n);
+	cout << (DP[n][0] + DP[n][1] + DP[n][2]);
+}
